e2424064bd5976f55bf60feb1aa28dc7c31ff7e6
nick-midmore <nmidmore@gmail.com>
2025-02-15 20:43:21 +0000

    added report_chars to main

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def count_chars(s):
         values[c] += 1
     return values
 
+def report_chars(d):
+    for k in sorted(d, key=d.get, reverse=True):
+        if k.isalpha():
+            print(f"'{k}' was found {d[k]} times")
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-        print(file_contents)
-        print(count_words(file_contents))
-        print(count_chars(file_contents))
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{count_words(file_contents)} words found in document")
+        report_chars(count_chars(file_contents))
+        print("--- End report ---")
 
 main()
